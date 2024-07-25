4436460e9ff39699217e0b7961cd535984647b6a
João Griebner <joao.griebner@gmail.com>
2024-07-25 16:31:02 -0300

    Add exercicio metodos

--- src/metodos/exercicios/ExAlunos.java
+++ src/metodos/exercicios/ExAlunos.java
@@ -1,6 +1,9 @@
 package metodos.exercicios;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import javax.swing.JOptionPane;
 
@@ -32,40 +35,62 @@ public class ExAlunos {
 		
 		ArrayList<Aluno> alunos = new ArrayList<Aluno>();
 		
-		int op = 0;
+		// CASO DE TESTE
 		
+//		Aluno a = new Aluno();
+//
+//		a.nome = "Pedro";
+//		a.notas.add(7.8);
+//		a.notas.add(8.6);
+//		a.notas.add(9.4);
+//		alunos.add(a);
+//		
+//		Aluno b = new Aluno();
+//
+//		b.nome = "Genivaldo";
+//		b.notas.add(4.3);
+//		b.notas.add(5.6);
+//		b.notas.add(6.7);
+//		alunos.add(b);
+//
+//		Aluno c = new Aluno();
+//
+//		c.nome = "Reprovadinho Junior";
+//		c.notas.add(1.1);
+//		c.notas.add(3.3);
+//		c.notas.add(2.6);
+//		alunos.add(c);
+//		
+		
+		int op = 0;
+
 		do {
 			op = menu();
 			switch (op) {
-			case 1: 
+			case 1:
 				cadastraAluno(alunos);
 				break;
 			case 2:
-				JOptionPane.showMessageDialog(null, listaAlunos(alunos));;
+				JOptionPane.showMessageDialog(null, listaAlunos(alunos));
 				break;
 			case 3:
-				JOptionPane.showMessageDialog(null, encontraPorNome(alunos)); // fazer funcionar
+				JOptionPane.showMessageDialog(null, mostraAluno(alunos, encontraPosicao(alunos)));
 				break;
 			case 4:
-				//listaAprovados();
+				JOptionPane.showMessageDialog(null, listaAprovados(alunos));
 				break;
 			case 5:
-				//listaRecuparacao();
+				JOptionPane.showMessageDialog(null, listaRecuperacao(alunos));
 				break;
 			case 6:
-				//listaReprovados();
+				JOptionPane.showMessageDialog(null, listaReprovados(alunos));
 				break;
-				
 
-			
-		}
+			}
 
-		} while(op != 9);
-		
+		} while (op != 9);
+
 	}
-
-
-
 
 	public static int menu() {
 		String m = "1 - Cadastrar alunos e suas notas\n" 
@@ -75,21 +100,21 @@ public class ExAlunos {
 				+ "5 - Listar alunos em Recuperação (mostrar nomes)\n"
 				+ "6 - Listar alunos Reprovados (mostrar nomes)\n\n"
 				+ "9 - SAIR";
-		
+
 		int op = Integer.parseInt(JOptionPane.showInputDialog(m));
-		
+
 		if ((op < 1 || op > 6) && op != 9) {
 			return menu();
 		}
 		return op;
 	}
-	
+
 	public static void cadastraAluno(ArrayList<Aluno> alunos) {
 
 		Aluno a = new Aluno();
-		
-		a.nome = JOptionPane.showInputDialog("Nome do aluno: ");	
-		
+
+		a.nome = JOptionPane.showInputDialog("Nome do aluno: ");
+
 		double nota = 0;
 
 		do {
@@ -102,43 +127,51 @@ public class ExAlunos {
 
 		alunos.add(a);
 	}
-	
+
 	public static double calculaMedia(ArrayList<Double> notas) {
 		double soma = 0;
 		for (Double nota : notas) {
 			soma += nota;
 		}
-		
-		return soma/notas.size();
+
+		return formataDouble(soma / notas.size());
 	}
-	
+
 	public static String verificaStatus(Aluno a) {
-			
-			double media = calculaMedia(a.notas);
-			
-			if (media >= 7) {
-				return "APROVADO";
-			} else if (media < 3){ 
-				return "REPROVADO";
-			}
-			return "EM RECUPERAÇÃO";
+
+		double media = calculaMedia(a.notas);
+
+		if (media >= 7) {
+			return "APROVADO";
+		} else if (media < 3) {
+			return "REPROVADO";
 		}
-	
-	public static String listaAlunos(ArrayList<metodos.exercicios.Aluno> alunos) {
+		return "EM RECUPERAÇÃO";
+	}
+
+	public static String listaAlunos(ArrayList<Aluno> alunos) {
 		String lista = "";
-		
-		for (Aluno a : alunos) {
-			lista += "Nome: " + a.nome + "\nMédia: " + calculaMedia(a.notas) + "\nStatus: " + verificaStatus(a) + "\n\n";
+
+		if (alunos.size() <= 0) {
+			lista = "Nenhum aluno cadastrado";
+		} else {
+			for (Aluno a : alunos) {
+				lista += "Nome: " + a.nome 
+						+ "\nMédia: " + calculaMedia(a.notas) 
+						+ "\nStatus: " + verificaStatus(a)
+						+ "\n\n";
+			}
+
 		}
-		
+
 		return lista;
 	}
-	
-	public static Aluno encontraPorNome(ArrayList<Aluno> alunos) {		
+
+	public static int encontraPosicao(ArrayList<Aluno> alunos) {
 		String nomeBusca = JOptionPane.showInputDialog("Nome para procurar:");
 		boolean nomeEncontrado = false;
 		int c = 0;
-		
+
 		do {
 			if (alunos.get(c).nome.equals(nomeBusca)) {
 				nomeEncontrado = true;
@@ -148,12 +181,97 @@ public class ExAlunos {
 		} while (c < alunos.size() && !nomeEncontrado);
 
 		if (!nomeEncontrado) {
-			return null ;
+			return -1;
 		}
-		
-		return alunos.get(c-1);
+
+		return c-1;
 	}
 	
+	public static String mostraAluno(ArrayList<Aluno> alunos, int pos) {
+		String msg = "";
+		
+		if (pos < 0) {
+			msg = "Aluno não encontrado";
+		} else {
+			msg = "Nome: " + alunos.get(pos).nome 
+				+ "\nMedia: " + calculaMedia(alunos.get(pos).notas)
+				+ "\nStatus: " + verificaStatus(alunos.get(pos))
+				+ "\n\n";
+		}
+		
+		return msg;
+	}
+
+	public static String listaAprovados(ArrayList<Aluno> alunos) {
+		String lista = "Lista de alunos aprovados: \n\n";
+
+		if (alunos.size() <= 0) {
+			lista = "Nenhum aluno cadastrado";
+		} else {
+
+			double media = 0;
+			
+			for (Aluno a : alunos) {
+				media = calculaMedia(a.notas);
+				if (media >= 7) {
+					lista += "Nome: " + a.nome + "\nMedia: " + media + "\n\n";
+				}
+			}
+
+		}
+
+		return lista;
+
+	}
 	
+	public static String listaRecuperacao(ArrayList<Aluno> alunos) {
+		String lista = "Lista de alunos em recuperacao: \n\n";
+
+		if (alunos.size() <= 0) {
+			lista = "Nenhum aluno cadastrado";
+		} else {
+
+			double media = 0;
+			
+			for (Aluno a : alunos) {
+				media = calculaMedia(a.notas);
+				if (media < 7 && media >= 3) {
+					lista += "Nome: " + a.nome + "\nMedia: " + media + "\n\n";
+				}
+			}
+
+		}
+
+		return lista;
+
+	}
+	
+	public static String listaReprovados(ArrayList<Aluno> alunos) {
+		String lista = "Lista de alunos reprovados: \n\n";
+
+		if (alunos.size() <= 0) {
+			lista = "Nenhum aluno cadastrado";
+		} else {
+
+			double media = 0;
+			
+			for (Aluno a : alunos) {
+				media = calculaMedia(a.notas);
+				if (media < 3 ) {
+					lista += "Nome: " + a.nome + "\nMedia: " + media + "\n\n";
+				}
+			}
+
+		}
+
+		return lista;
+
+	}
+	
+	public static double formataDouble(double n) {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+        DecimalFormat df = new DecimalFormat("#.00", symbols); 
+        return Double.parseDouble(df.format(n));
+    }
 	
 }
